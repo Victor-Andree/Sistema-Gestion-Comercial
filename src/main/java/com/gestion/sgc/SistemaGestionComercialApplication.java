ae457ab7d2971d81a6e6e08cede2d61e48de094f
Victor <u21221311@utp.edu.pe>
2025-09-12 12:52:07 -0500

    first commit

---
+++ src/main/java/com/gestion/sgc/SistemaGestionComercialApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.sgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionComercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionComercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/sgc/SistemaGestionComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.sgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
